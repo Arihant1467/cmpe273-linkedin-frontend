c881d07d928f6b78ec777395685c7ad3e3cac6f8
admin <admin@USCS-Mac185.local>
2019-01-05 21:24:00 -0800

    removed redux dev tool

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,11 +1,36 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware,compose} from 'redux';
 import storeManager from './../../reducers/allreducer.js';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || window.__REDUX_DEVTOOLS_EXTENSION__();
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(
   storeManager,
-  composeEnhancer(applyMiddleware(thunk, promise)),
+  compose(applyMiddleware(thunk, promise)),
 );
+
+/*
+const middleware = [thunk]
+
+/*
+const store = createStore(
+    rootReducer,
+    appState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+
+const store = createStore(
+  rootReducer,
+  appState,
+  compose(
+      applyMiddleware(...middleware)
+  )
+);
+
+*/
+
 export default store;
